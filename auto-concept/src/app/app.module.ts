bfa260b66968375381d9a136f4e460b821d28aba
VNemyrovskyi <Vitalii.Nemyrovskyi@onseo.biz>
2016-12-01 20:49:37 +0200

    Added  bootstrap

--- auto-concept/src/app/app.module.ts
+++ auto-concept/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
