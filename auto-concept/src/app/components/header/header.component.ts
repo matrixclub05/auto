2b0faf4d652f557d574cdfa3288e814c4bdbfb51
VNemyrovskyi <Vitalii.Nemyrovskyi@onseo.biz>
2016-12-02 20:34:05 +0200

    refactor

--- auto-concept/src/app/components/header/header.component.ts
+++ auto-concept/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {RegistrationFlowComponent} from "../../registration/registrationFlow/registration-flow.component";
 
 @Component({
   selector: '[app-header]',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _modalService:NgbModal) { }
 
   ngOnInit() {
   }
-
+  public openRegistrationModal()
+  {
+    this._modalService.open(RegistrationFlowComponent);
+  }
 }
